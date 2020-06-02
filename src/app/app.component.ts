5c4db816267cb625da46f36ce451afc4d3c8018b
Manjushree S Padmasali <ManjushreeP@quinnox.com>
2020-06-02 15:53:39 +0530

    Manjushree-AngularApp

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+import {NumberFormatterComponent} from './number-formatter.component';
+import {NumericEditorComponent} from './numeric-editor.component';
+import {RangeFilterComponent} from './range-filter.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  columnDefs = [
+    {headerName: 'Make', field: 'make'},
+    {headerName: 'Model', field: 'model'},
+    {
+      headerName: 'Price',
+      field: 'price',
+      editable: true,
+      cellRenderer: 'numberFormatterComponent',
+      cellEditor: 'numericEditorComponent',
+      filter: 'rangeFilterComponent'
+    }
+  ];
+
+  rowData = [ { make: 'Toyota', model: 'Celica', price: 35000 },
+  { make: 'Ford', model: 'Mondeo', price: 32000 },
+  { make: 'Porsche', model: 'Boxter', price: 72000 }];
+
+  frameworkComponents = {
+    numberFormatterComponent: NumberFormatterComponent,
+    numericEditorComponent: NumericEditorComponent,
+    rangeFilterComponent: RangeFilterComponent
+  };
+
+  ngOnInit() {
+    fetch('https://api.myjson.com/bins/15psn9')
+      .then(result => result.json())
+      .then(rowData => this.rowData = rowData);
+  }
+}
